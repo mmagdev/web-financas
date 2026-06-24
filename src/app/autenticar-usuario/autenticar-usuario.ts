c62167b980d40450f24058772cae805153ac5295
unknown <matheusmag.dev@gmail.com>
2026-06-24 13:05:55 -0300

    Integração com a API de autenticação

---
+++ src/app/autenticar-usuario/autenticar-usuario.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-autenticar-usuario',
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './autenticar-usuario.html',
+  styleUrl: './autenticar-usuario.css',
+})
+export class AutenticarUsuario {
+
+  //declarar um objeto  do tipo HttpClient
+  private http = inject(HttpClient);
+
+  //Objeto para capturar o formuário de autenticação
+  formAutenticar = new FormGroup({
+    email : new FormControl('', [Validators.required]),
+    senha : new FormControl('', [Validators.required]),
+
+  });
+
+  //Função para fazer a requisição para a API.
+  //Será executada quando o botão "submit" for clicado
+  autenticar() {
+      //Enviando uma requisição HTTP POST para a API
+      this.http.post('http://localhost:8082/api/v1/usuarios/autenticar', this.formAutenticar.value)
+      .subscribe((response) => {
+        console.log(response);
+      });
+    }
+}
